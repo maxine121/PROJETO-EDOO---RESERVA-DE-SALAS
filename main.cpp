125e5bac7a5176081695f6e701d76b11aec48727
Ítalo Borges <italo.borges1211@gmail.com>
2025-10-22 09:23:00 -0300

    Delete main.cpp

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "headers/sala.h"
-int main() {
-    Sala lab1(1, "laboratorio");
-    std::cout << "id: " << lab1.getId() << std::endl;
-    std::cout << "Tipo: " << lab1.getTipo() << std::endl;
-    return 0;
-}
